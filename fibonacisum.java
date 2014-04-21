388e439982ee59254dfaf63686d50455cf3d9675
MARTIN LUTER <m.luter@rocketmail.com>
2014-04-21 17:27:40 +0700

    fibonacisum.java
    fibonacisum.java yang baru

---
+++ fibonacisum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class fibonaci {
+	public static void main (String args[]) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Masukkan inputan anda: ");
+		int l=0; int r=1;
+		int p = s.nextInt();
+		for (int i=1; i<=p; i++) {
+			r=r+l;
+			l=r-l;
+		}
+		System.out.print(r+l-1);
+	}
+}
