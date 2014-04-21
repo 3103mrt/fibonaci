71cb92e307eb59c03edc4c63fc0ec72cdf41470c
MARTIN LUTER <m.luter@rocketmail.com>
2014-04-21 17:44:56 +0700

    Update fibonacisum.java

--- fibonacisum.java
+++ fibonacisum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class fibonaci {
+public class fibonacisum {
 	public static void main (String args[]) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Masukkan inputan anda: ");
